026a3e56150e44622f9b72f3d1ae34f9e98b39bb
Nathanael Wung <157310799+nathwung@users.noreply.github.com>
2024-10-05 01:17:38 -0400

    Create ViewStats.vue

---
+++ ViewStats.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="stats">
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>Stat</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Number of Notes</td>
+                    <td>{{ storeNotes.getTotalNotes() }}</td>
+                </tr>
+                <tr>
+                    <td>Number of Characters (of all notes)</td>
+                    <td>{{ storeNotes.getTotalCharacters() }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+import {useStoreNotes} from '@/stores/storeNotes'
+
+const storeNotes = useStoreNotes()
+
+</script>
