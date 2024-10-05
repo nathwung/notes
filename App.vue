a5c5e96ea40811ef0f068a4ca2f24ecd29f23b63
Nathanael Wung <157310799+nathwung@users.noreply.github.com>
2024-10-05 01:18:11 -0400

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from '@/components/Layout/NavBar.vue'
+import { useStoreAuth } from '@/stores/storeAuth'
+
+onMounted(()=>{
+  const storeAuth = useStoreAuth()
+  storeAuth.init()
+})
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
+
+</template>
+
+<style>
+
+@import 'bulma/css/bulma.min.css';
+
+</style>
